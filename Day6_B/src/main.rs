c9088312d32843ceb1c177d2d1ca0bc08be15be0
sdrauksas <71008456+sdrauksas@users.noreply.github.com>
2022-12-06 17:10:02 +0200

    Day 6 - Change four_chars to fourteen_chars

--- Day6_B/src/main.rs
+++ Day6_B/src/main.rs
@@ -7,10 +7,10 @@ fn main() {
     // Split input into crate stacks and commands
     for line in file_contents.lines() {
         for i in 14..line.chars().count() {
-            let mut four_chars: Vec<char> = line[i-14..i].chars().collect();
-            four_chars.sort();
-            four_chars.dedup();
-            if four_chars.len() == 14 {
+            let mut fourteen_chars: Vec<char> = line[i-14..i].chars().collect();
+            fourteen_chars.sort();
+            fourteen_chars.dedup();
+            if fourteen_chars.len() == 14 {
                 result = i;
                 break;
             }
